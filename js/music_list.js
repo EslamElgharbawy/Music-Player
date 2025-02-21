4b89324558d4b765509a1327bcec3ca18de5d7fa
EslamElgharbawy <eslamelsayd334@gmail.com>
2025-02-21 20:51:06 +0200

    change image

--- js/music_list.js
+++ js/music_list.js
@@ -2,28 +2,28 @@ let AllMusic = [
     {
         name: "Sweater Weather",
         artist: "Neighbourhood",
-        img: "../images/img4.jpg",
+        img: "https://raw.githubusercontent.com/EslamElgharbawy/Music-Player/main/images/img4.jpg",
         src: "https://raw.githubusercontent.com/EslamElgharbawy/Music-Player/main/Songs/Song4.mp3",
 
     },
     {
         name: "Blinding Lights",
         artist: "Weeknd",
-        img: "../images/img3.jpg",
+        img: "https://raw.githubusercontent.com/EslamElgharbawy/Music-Player/main/images/img3.jpg",
         src: "https://raw.githubusercontent.com/EslamElgharbawy/Music-Player/main/Songs/Song3.mp3",
 
     },
     {
         name: "Surfin' Bird",
         artist: "Trashmen",
-        img: "../images/img1.jpg",
+        img: "https://raw.githubusercontent.com/EslamElgharbawy/Music-Player/main/images/img1.jpg",
         src: "https://raw.githubusercontent.com/EslamElgharbawy/Music-Player/main/Songs/Song1.mp3",
 
     },
     {
         name: "Somebody's Watching Me",
         artist: "Rockwell",
-        img: "../images/img2.jpg",
+        img: "https://raw.githubusercontent.com/EslamElgharbawy/Music-Player/main/images/img2.jpg",
         src: "https://raw.githubusercontent.com/EslamElgharbawy/Music-Player/main/Songs/Song2.mp3",
 
     },
